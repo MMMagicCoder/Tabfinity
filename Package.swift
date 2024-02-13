955a4daeee59dc87111182b14525809f686b7f5e
Mohammad Mahdi Moayeri <mmahdimoayeri@gmail.com>
2024-02-13 20:41:17 +0330

    Update README.md

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "TabLight",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TabLight",
             targets: ["TabLight"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TabLight"),
-        .testTarget(
-            name: "TabLightTests",
-            dependencies: ["TabLight"]),
+            name: "TabLight",
+            dependencies:[])
     ]
 )
